d5a542fe1a0306a273acdfe394170b2786d00f2a
Jay Leadbetter <jay.leadbetter@gmail.com>
2014-02-11 14:16:47 -0700

    Added models and fields

---
+++ null_reality/fields.py
@@ -0,0 +1,53 @@
+"""
+Text-based fields that respect null=False. Available fields include:
+
+* NullableCharField
+* NullableCommaSeparatedIntegerField
+* NullableEmailField
+* NullableSlugField
+* NullableTextField
+* NullableURLField
+
+Please use these fields with a NullCheckerModel child class
+"""
+
+from django.db import models
+
+
+class NullableField(models.Field):
+    """
+    Eliminates Django's tendency to override nulls with empty strings when you
+    want a model field to respect null=False. Should be used in conjunction 
+    with a child class of NullCheckerModel.
+    """
+    def get_default(self):
+        if not self.null:
+            return None
+        
+        # if self.null is True, will still set value to empty string to 
+        # prevent issues with the database
+        return super(NullableField, self).get_default()
+
+
+class NullableCharField(models.CharField, NullableField):
+    pass
+
+
+class NullableCommaSeparatedIntegerField(models.CommaSeparatedIntegerField, NullableField):
+    pass
+
+
+class NullableEmailField(models.EmailField, NullableField):
+    pass
+
+
+class NullableSlugField(models.SlugField, NullableField):
+    pass
+
+
+class NullableTextField(models.TextField, NullableField):
+    pass
+
+
+class NullableURLField(models.URLField, NullableField):
+    pass
---
+++ null_reality/models.py
@@ -0,0 +1,29 @@
+from django.db import models, IntegrityError
+
+from null_reality.fields import NULLABLE_FIELDS
+
+
+class NullCheckerModel(models.Model):
+    """
+    By default, Django does not respect the null=False requirement on CharField
+    and instead inserts an empty string.  For sane people who actually want
+    CharFields to throw IntegrityErrors if the value for the field is None,
+    use this class.
+    
+    Notes:
+        If you use Django's default text-based fields (e.g., CharField,
+        TextField) with NullCheckerModel, it will respect Django's default
+        behaviors for those fields. To get the null checking behaviors 
+        promised by this class, please use the nullable field classes in
+        fields.py
+    """
+    def save(self, *args, **kwargs):
+        for field in self._meta.fields:
+            if type(field) in NULLABLE_FIELDS:
+                if not field.null and getattr(self, field.name) is None:
+                    raise IntegrityError("%s.%s may not be null" %
+                                         (self._meta.db_table, field.name))
+        super(AbstractNullCheckerModel, self).save(*args, **kwargs)
+    
+    class Meta:
+        abstract = True
